1cafbee441aab9d1d12e9d79664194b24fbef565
Joyce Chan <joycehwchan@gmail.com>
2022-10-22 15:39:19 +0900

    add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments
 
-  validates :username, presence: true, length: { in: 6..16 }, uniqueness: true
+  validates :username, presence: true, length: { in: 4..20 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
